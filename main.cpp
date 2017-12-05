2a3e5a99d3a00bc88eb2179d4f41e73c2a0506a8
rramos20 <rramos20@mail.csuchico.edu>
2017-12-04 18:12:55 -0800

    I can't get it to display a vector of drinks

--- main.cpp
+++ main.cpp
@@ -2,42 +2,59 @@
 #include<string>
 #include<vector>
 #include "textfile.h"
-
+#include "drinkmath.h"
+#include <fstream>
 using namespace std;
 
 int main()
 {
+drinkmath beverage;
 textfile fileAction;
 char option;
 cout<<"Would you like to start a new file or open an existing file?\n(N)ew/(O)pen/(C)lose..."<<endl;
-
-while((option != 'O'||'o') && (option != 'N'||'n') && (option != 'C'||'c'))
+cin>>option;
+while((option != 'O') && (option != 'o') && (option != 'N') && (option != 'n') && (option != 'C') && (option !='c'))
     {
     cout<<"Please enter a valid option."<<endl;
     cin>>option;
     }
-if(option == 'O'||'o')
+
+
+if((option == 'O') || (option =='o'))
     {
     string previousFile;
     cout<<"What is your file name?"<<endl;
     cin>>previousFile;
 
     fileAction.accessFile(previousFile);
-
     }
-else if(option == 'N'||'n')
+
+
+else if((option == 'N') || (option == 'n'))
     {
+    int drinks;
     string firstName,lastName;
     cout<<"What is your full name?"<<endl;
     cin>>firstName>>lastName;
     fileAction.setName(firstName,lastName);
     fileAction.createFile(fileAction.fullName());
-
+    cout<<"Hello, "<<firstName + "!"<<endl;
+    vector <int> weeklyDrinks;
+    do
+    {   
+        cout<<"How many alcoholic beverages have you had each day of this week? (Enter -1 to finish)"<<endl;
+        cin>>drinks;
+        
+    }while(drinks > -1);
+        {
+        beverage.addDrinks(drinks);
+        }
     }
-else(option == 'C'||'c');
 
 
+else((option == 'C') || (option != 'c'));
 
+    
 
 
 
